0ac5db2670c115f9a9ec0d08505255efce2ab43d
Bean <beansobre@gmail.com>
2023-04-10 05:44:12 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Create a new application builder
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // This is like adding tools to a toolbox.
+        builder.Services.AddControllers(); // Adds the ability to handle requests and return responses
+        builder.Services.AddEndpointsApiExplorer(); // Adds a tool to help explore the API
+        builder.Services.AddSwaggerGen(); // Adds a tool to generate API documentation
+
+        // Build the application
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        // This is like setting up the assembly line for making something.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger(); // Use the API documentation tool
+            app.UseSwaggerUI(); // Use the API explorer tool
+        }
+
+        app.UseHttpsRedirection(); // Redirect all HTTP requests to HTTPS
+
+        app.UseAuthorization(); // Check if the user is authorized to access the API
+
+        app.MapControllers(); // Map requests to the appropriate controller method
+
+        app.Run(); // Start the application
+    }
+}
